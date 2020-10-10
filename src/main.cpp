701a3e17bde1e7f51050c13041cd5e13d1d4188e
Siroyan <1517053@ed.tus.ac.jp>
2020-10-11 01:37:54 +0900

    カウントダウン機能の追加

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #include <LovyanGFX.hpp>
 #include <Credentials.h>
 #include <HttpCommunication.h>
+#include "time.h"
 
 static LGFX lcd;
 static LGFX_Sprite sprite(&lcd);
@@ -15,13 +16,17 @@ static LGFX_Sprite sprite(&lcd);
 bool httpCommunication(char* url, char* server);
 DynamicJsonDocument convertToJson(String receivedText);
 void displayThumbnail(DynamicJsonDocument searchResultJson);
-void displayTimer(DynamicJsonDocument searchResultJson);
+void getLiveStartTime(DynamicJsonDocument searchResultJson);
+void displayTimer();
+long convertToUnixTime(const char* iso8601Time);
 
 const char* ssid = WIFI_SSID;
 const char* password = WIFI_PW;
 
+const char* ntpServer = "ntp.nict.jp";
+
 const char* apiKey = API_KEY;
-const char* ytSearchApiUrlNoKey = "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCD-miitqNY3nyukJ4Fnf4_A&eventType=upcoming&maxResults=1&type=video&order=date&key=";
+const char* ytSearchApiUrlNoKey = "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCjlmCrq4TP1I4xguOtJ-31w&eventType=upcoming&maxResults=1&type=video&order=date&key=";
 const char* ytVideoApiUrlNoIdNoKey = "https://www.googleapis.com/youtube/v3/videos?part=liveStreamingDetails&id=";
 const char* ytServer = "www.googleapis.com";
 const char* imageServer = "i.ytimg.com";
@@ -36,21 +41,21 @@ void setup() {
   // LCD Setting
   lcd.init();
   lcd.setRotation(1);
-  lcd.setBrightness(100);
+  lcd.setBrightness(128);
+  lcd.setColorDepth(24);
   lcd.clear();
+  lcd.setFont(&fonts::lgfxJapanGothic_36);
   
   // Serial comm to PC
   Serial.begin(115200);
-  while(!Serial);
   delay(1000);
 
   // Wi-Fi
   WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    Serial.print(".");
-    delay(1000);
-  }
+  while (WiFi.status() != WL_CONNECTED);
   Serial.println("Connected to " + String(ssid));
+
+  configTime(0, 0, ntpServer);
 }
 
 void loop() {
@@ -64,14 +69,14 @@ void loop() {
       // 配信予定があるか
       if (searchResultJson["items"] != NULL) {
         displayThumbnail(searchResultJson);
-        displayTimer(searchResultJson);
+        getLiveStartTime(searchResultJson);
         // カウントダウンモードへ移行
         if (YtThumbnail.isSucceeded() && YtVideoApi.isSucceeded()) mode = 1;
       }
     }
-    delay(10000);
   } else if (mode == 1){
     // 配信までのカウントダウン
+    displayTimer();
   }
 }
 
@@ -93,25 +98,61 @@ void displayThumbnail(DynamicJsonDocument searchResultJson) {
   }
 }
 
-void displayTimer(DynamicJsonDocument searchResultJson) {
+void getLiveStartTime(DynamicJsonDocument searchResultJson) {
   // 配信予定の詳細を取得
   const char* id = searchResultJson["items"][0]["id"]["videoId"];
   String ytVideoApiUrl = String(ytVideoApiUrlNoIdNoKey) + String(id) + "&key=" +  String(apiKey);
 
   YtVideoApi.setup((char*)ytVideoApiUrl.c_str(), (char*)ytServer);
   if (YtVideoApi.request()) {
+    // 配信開始時間を取得
     String recievedLiveDetail = YtVideoApi.getRecievedData();
     DynamicJsonDocument upComingLiveDetailJson = convertToJson(recievedLiveDetail);
     scheduledStartTime = upComingLiveDetailJson["items"][0]["liveStreamingDetails"]["scheduledStartTime"];
-    String year = String(scheduledStartTime).substring(0, 4);
-    String month = String(scheduledStartTime).substring(5, 7);
-    String day = String(scheduledStartTime).substring(8, 10);
-    String hour = String((String(scheduledStartTime).substring(11, 13)).toInt() + 9);
-    String minits = String(scheduledStartTime).substring(14, 16);
-    lcd.setTextSize(2.5);
-    lcd.drawString("This live start at", 10, M_IMG_HEIGHT + 10); 
-    lcd.drawString(year + "/" + month + "/" + day + " " + hour + ":" + minits, 10, M_IMG_HEIGHT + 35);
   }
+}
+
+void displayTimer() {
+  // 現在時刻から配信開始までの時間を計算
+long diffTime = convertToUnixTime(scheduledStartTime) - time(NULL);
+  if (diffTime > 24 * 60 * 60) {
+    // 一日以上後
+    lcd.drawString("一日以上先の配信です", 10, M_IMG_HEIGHT + 10);
+  } else if (diffTime <= 0) { 
+    lcd.drawString("配信中!", 100, M_IMG_HEIGHT + 10);
+  } else {
+    uint8_t diffH = diffTime / 3600;
+    uint8_t diffM = (diffTime % 3600) / 60;
+    uint8_t diffS = diffTime % 60;
+    String h = diffH < 10 ? "0" + String(diffH) : String(diffH);
+    String m = diffM < 10 ? "0" + String(diffM) : String(diffM);
+    String s = diffS < 10 ? "0" + String(diffS) : String(diffS);
+    lcd.drawString("開始まで " + h + ":" + m + ":" + s, 5, M_IMG_HEIGHT + 10);
+  }
+}
+
+long convertToUnixTime(const char* iso8601Time) {
+  uint16_t totalDays[12] = {0,31,59,90,120,151,181,212,243,273,304,334};
+
+  uint16_t y  = String(iso8601Time).substring( 0,  4).toInt();
+  uint8_t  mo = String(iso8601Time).substring( 5,  7).toInt();
+  uint8_t  d  = String(iso8601Time).substring( 8, 10).toInt();
+  uint8_t  h  = String(iso8601Time).substring(11, 13).toInt();
+  uint8_t  mi = String(iso8601Time).substring(14, 16).toInt();
+  uint8_t  s  = String(iso8601Time).substring(17, 19).toInt();
+
+  long unixTime = 0;
+  for (uint16_t year = 1970; year < y; year++) {
+    if ((year % 400) == 0 || ((year % 4) == 0 && (year % 100) != 0)) unixTime += 366 * 24 * 60 * 60;
+    else unixTime += 365 * 24 * 60 * 60;
+  }
+  unixTime += totalDays[mo-1] * 24 * 60 * 60;
+  unixTime += d * 24 * 60 * 60;
+  unixTime += h * 60 * 60;
+  unixTime += mi * 60;
+  unixTime += s;
+
+  return unixTime;
 }
 
 DynamicJsonDocument convertToJson(String receivedText) {
